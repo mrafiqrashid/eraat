e2b2c6db1e2425f6a95c99a2d0fb5e1ad371158a
mrafiqrashid <rrafiqofficial@gmail.com>
2025-05-31 21:35:48 +0800

    fix: snappy config

--- config/snappy.php
+++ config/snappy.php
@@ -35,8 +35,8 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        // 'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
-        'binary' => '"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"',
+        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        // 'binary' => '"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"',
         'timeout' => false,
         'options' => [],
         'env'     => [],
